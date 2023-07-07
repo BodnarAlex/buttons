bfe6fd6f6cb9cfc6d6a68bf9fad345573b376dd4
Bodnar Alex <allugovskova@mail.ru>
2023-07-07 14:34:42 +0200

    feat: switch for buttons

--- script/script.js
+++ script/script.js
@@ -5,14 +5,43 @@ $(window).on('load', function() {
 
     //слушатель на кнопки цветов
     $('.colour_item').click(function(){
-        console.log(13);
         const list = this.classList;
-        var color;
         if(list.length==2){
-            color = list[1];
+            var color = list[1];
             ChangeColor(color)
         }
     });  
+
+    $('.button').click(function(){
+        const list = this.classList;
+        console.log(this);
+       switch (list[1]){
+        case 'change_color':
+            console.log(1);
+            break;
+        case 'change_buttons':
+            console.log(2);
+            break;
+        case 'change_nav':
+            console.log(3);
+            break;
+        case 'on_music':
+            console.log(4);
+            break;
+        case 'delete_button':
+            console.log(5);
+            break;
+        case 'fly_header':
+            console.log(6);
+            break;
+        case 'strong_button':
+            console.log(7);
+            break;
+        case 'boom_everything':
+            console.log(8);
+            break;
+       }
+    });
 
     //функция изменения цвета
     function ChangeColor(color){
